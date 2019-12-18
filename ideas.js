36e21c1598ce3388a0af2783b429f4ab8b95f6cf
surpol <suryapolina@gmail.com>
2019-12-17 23:11:47 -0500

    Window size adjustment
    Failed

---
+++ ideas.js
@@ -0,0 +1,6 @@
+//var button = document.getElementById("get_idea");
+//button.onclick = print();
+//var button2 = document.getElementById("get_idea")
+function hi(){
+	document.getElementById(demo).innerHTML = "\nHello World";
+}
