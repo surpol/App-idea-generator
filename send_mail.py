e68e287ff9e47294bf3cc9aea647ddb195986a36
surpol <suryapolina@gmail.com>
2019-12-23 20:32:24 -0500

    adding suryapolina2 email

--- send_mail.py
+++ send_mail.py
@@ -9,7 +9,8 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'namskek2@gmail.com'
 app.config['MAIL_PASSWORD'] = 'coolBeans22'
-
+app.config['MAIL_USERNAME'] = 'suryapolina2@gmail.com'
+app.config['MAIL_PASSWORD'] = 'myWare99'
 mail = Mail(app)
 
 @app.route('/')
@@ -22,7 +23,7 @@ def send():
 		name = request.form['name']
 		email = request.form['email']
 		feedback = request.form['feedback']
-		msg = Message(sender=email, recipients=['namskek2@gmail.com'], body=feedback, subject=name)
+		msg = Message(sender=email, recipients=['namskek2@gmail.com', 'suryapolina2@gmail.com'], body=feedback, subject=name)
 		mail.send(msg)
 		return render_template('feedbackMessage.html', name=name)
 	else: 
@@ -32,7 +33,7 @@ def send():
 def idea():
 	if request.method == 'POST':
 		idea = request.form['idea']
-		msg = Message(sender='namskek2@gmail.com', recipients=['namskek2@gmail.com'],body=idea, subject='New Idea Submission')
+		msg = Message(sender='namskek2@gmail.com', recipients=['namskek2@gmail.com', 'suryapolina2@gmail.com'],body=idea, subject='New Idea Submission')
 		mail.send(msg)
 		return render_template('ideaMessage.html')
 	else:
