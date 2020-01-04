efa842b13be172c4998a1557977604c1c3c16804
Surya Polina <45124625+suryapolina99@users.noreply.github.com>
2020-01-04 16:36:47 -0500

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -7,14 +7,11 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'namskek2@gmail.com'
-app.config['MAIL_PASSWORD'] = 'coolBeans22'
-app.config['MAIL_USERNAME'] = 'suryapolina2@gmail.com'
-app.config['MAIL_PASSWORD'] = 'myWare99'
-# app.config['MAIL_USERNAME'] = 'email'
-# app.config['MAIL_PASSWORD'] = 'email password'
-# app.config['MAIL_USERNAME'] = 'email'
-# app.config['MAIL_PASSWORD'] = 'email password'
+
+app.config['MAIL_USERNAME'] = 'email'
+app.config['MAIL_PASSWORD'] = 'email password'
+app.config['MAIL_USERNAME'] = 'email'
+app.config['MAIL_PASSWORD'] = 'email password'
 mail = Mail(app)
 
 @app.route('/')
@@ -27,9 +24,9 @@ def send():
 		name = request.form['name']
 		email = request.form['email']
 		feedback = request.form['feedback']
-		msg = Message(sender=email, recipients=['namskek2@gmail.com', 'suryapolina2@gmail.com'], body=feedback, subject=name)
+		msg = Message(sender=email, recipients=['email', 'email'], body=feedback, subject=name)
 		feedback = "Sent by: " + email + "\n" + "Feedback: " + request.form['feedback']
-		msg = Message(sender=email, recipients=['suryapolina2@gmail.com', 'namskek2@gmail.com'], body=feedback, subject=name)
+		msg = Message(sender=email, recipients=['email', 'email'], body=feedback, subject=name)
 		mail.send(msg)
 		return render_template('feedbackMessage.html', name=name)
 	else: 
@@ -40,10 +37,10 @@ def idea():
 	if request.method == 'POST':
 		appName = request.form['appName']
 		idea = request.form['idea']
-		msg = Message(sender='namskek2@gmail.com', recipients=['namskek2@gmail.com', 'suryapolina2@gmail.com'],body=idea, subject='New Idea Submission')
+		msg = Message(sender='email', recipients=['email', 'email'],body=idea, subject='New Idea Submission')
 		ideaEmail = request.form['ideaEmail']
 		submission = "Sent by: " + ideaEmail + "\n" + "App Name: " + appName + "\n" + "Idea Description: " + idea
-		msg = Message(sender='namskek2@gmail.com', recipients=['namskek2@gmail.com', 'suryapolina2@gmail.com'],body=submission, subject='New Idea Submission')
+		msg = Message(sender='email', recipients=['email', 'email'],body=submission, subject='New Idea Submission')
 		mail.send(msg)
 		return render_template('ideaMessage.html')
 	else:
